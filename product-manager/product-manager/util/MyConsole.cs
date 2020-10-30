3c6d309f109849363187c893db44fc919a12ccfb
Sean Blessing <snblessing@gmail.com>
2020-10-30 13:46:41 -0400

    added MyConsole

---
+++ product-manager/product-manager/util/MyConsole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace product_manager.util {
+    class MyConsole {
+        static String GetString(String prompt) {
+            Console.WriteLine(prompt);
+            String s = Console.ReadLine();  // read user entry
+            return s;
+        }
+        public static double getDouble(String prompt) {
+            double d = 0;
+            bool isValid = false;
+            while (!isValid) {
+                Console.Write(prompt);
+                try { 
+                    d = double.Parse(Console.ReadLine());
+                    isValid = true;
+                }
+                catch(Exception e) {
+                    Console.WriteLine("Error! Invalid double. Try again.");
+                }
+            }
+            return d;
+        }
+
+    }
+
+}
