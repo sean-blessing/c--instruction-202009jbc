52208a37d1005ec0740ebbbe85d01426f9fa04ac
Sean Blessing <snblessing@gmail.com>
2020-10-30 10:44:00 -0400

    Chapter 4 and 5 class demos

---
+++ ch04-demo/ch04-demo1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ch04_demo1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Doing more control statements!");
+
+            Console.Write("Enter a dwarf #:  ");
+            int nbr = Int32.Parse(Console.ReadLine());
+
+            switch (nbr) {
+                case 1:
+                    Console.WriteLine("Sleepy");
+                    break;
+                case 2:
+                    Console.WriteLine("Dopey");
+                    break;
+                case 3:
+                    Console.WriteLine("Bashful");
+                    break;
+                case 4:
+                    Console.WriteLine("Grumpy");
+                    break;
+                case 5:
+                    Console.WriteLine("Happy");
+                    break;
+                case 6:
+                    Console.WriteLine("Doc");
+                    break;
+                case 7:
+                    Console.WriteLine("Sneezey");
+                    break;
+                default:
+                    Console.WriteLine("Invalid #.");
+                    break;
+            }
+
+
+
+
+        }
+
+
+    }
+}
---
+++ ch05-demo/ch05-demo1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ch05_demo1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Chapter 5 demos!");
+
+            int nbr = ParseInt("Enter a whole number: ");
+
+            Console.WriteLine("you entered: " + nbr);
+
+            PrintOrderDetails(productName: "Light Up Pumpkin", orderNum: 33,
+                sellerName:"Target");
+
+            PrintCandyStats("Snickers", 5);
+            PrintCandyStats("KitKat");
+            PrintFavSong("Accidents Will Happen");
+
+        }
+
+        private static int ParseInt(String prompt) {
+            int n = 0;
+            while (true) {
+                Console.WriteLine(prompt);
+                try {
+                    n = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception) {
+                    Console.WriteLine("Hey!  Enter something valid!");
+                }
+            }
+            return n;
+        }
+
+        // demonstrate named parameters
+        private static void PrintOrderDetails(String sellerName,
+                            int orderNum, String productName) {
+            Console.WriteLine("Order Details: " + productName + ", on " +
+                            "order # " + orderNum + ", purchased at " + 
+                            sellerName + ".");
+        }
+
+        // demonstrate optional parameters
+        private static void PrintCandyStats(String candyName, 
+                                            int quantity = 0) {
+            Console.WriteLine("* " + quantity + " pieces of " 
+                                + candyName);
+        }
+
+        private static void PrintFavSong(String name) {
+            //Console.WriteLine("My favorite song is " + name + ".");
+            Console.WriteLine($"My favorite song is {name}.");
+
+        }
+    }
+}
