16a989f2dee00a5cc89662e5840e9c2e8bc2f94c
Sean Blessing <snblessing@gmail.com>
2020-10-30 14:23:17 -0400

    chapter 11 demos

---
+++ ch11-demo/ch11-demo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ch11_demo1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Arrays Demo!");
+
+            int[] nbrs = { 2, 4, 6, 8, 10, 50, 100 };
+            double[] prices = new double[3];
+            prices[0] = 11.99;
+            prices[1] = 55.99;
+            prices[2] = 2.99;
+
+            foreach (int n in nbrs) {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("2nd element in prices: " + prices[1]);
+        }
+    }
+}
